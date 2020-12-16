e5e0977be56cd7fbadaf5c401eafac10a23ed695
debs-ilegra <deborawendland@gmail.com>
2020-12-16 18:37:06 -0300

    change count base pairs

--- computational-molecular-biology/download_genome_pipeline.sh
+++ computational-molecular-biology/download_genome_pipeline.sh
@@ -1,4 +1,4 @@
-directory=atividade-01
+directory=atividade-01-parte-01
 fasta_name=all_chromosomes.fa
 mkdir $directory
 cd $directory
@@ -11,30 +11,19 @@ do
 done
 
 echo "downloading mt chromossome..."
-wget ${url/replace/MT}
-
+wget ${url/replace/MT} -q --show-progress
 echo "unzipping .gz files..."
 gunzip $(ls)
 
 touch $fasta_name
-
 echo "adding files content to " $fasta_name "..."
 for filename in $(ls);
 do
-  echo $filename
   cat $filename >> $fasta_name
 done
 
-echo "counting base pairs in chromosomes..."
-for filename in $(ls);
-do
-  substring=$(sed -e 's/REF\(.*\)>/\1/' <<< $(cat $filename)) #Entre REF e >, para cada repeticao
-  echo "Genoma lenght: " ${#substring}
-done
-
 echo "counting base pairs in chromosomes of " $fasta_name "..."
-substring=$(sed -e 's/REF\(.*\)>/\1/' <<< $(cat fasta_name)) #Entre REF e >, para cada repeticao
-echo "Genoma lenght: " ${#substring}
+cat $fasta_name | sed '/>/d' | wc -c
 
 cds_all=http://ftp.ensembl.org/pub/release-93/fasta/danio_rerio/cds/Danio_rerio.GRCz11.cds.all.fa.gz
 echo "downloading and unzipping" $cds_all "..."
@@ -42,7 +31,3 @@ wget $cds_all -q --show-progress
 gunzip Danio_rerio.GRCz11.cds.all.fa.gz
 
 echo "Gene count: " $(cat Danio_rerio.GRCz11.cds.all.fa | tr -cd '>' | wc -c)
-
-
-
-
